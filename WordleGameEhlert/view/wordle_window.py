460720cb0afbaea8ed1658dd7a5e484245f15a8a
Tony Ehlert <aehlert@dmacc.edu>
2023-04-24 16:52:48 -0500

    Updated for WordleGame object. Game functioning with submit button. Still needs letter label changes implemented

--- WordleGameEhlert/view/wordle_window.py
+++ WordleGameEhlert/view/wordle_window.py
@@ -10,195 +10,54 @@ The output is a functional Wordle game GUI
 import random
 #imports
 import tkinter as tk
-from tkinter import SUNKEN, CENTER
+from tkinter import SUNKEN, CENTER, NORMAL, DISABLED
 
+from WordleGameEhlert.model.WordleGame import WordleGame
 from WordleGameEhlert.model.Attempt import Attempt
 from WordleGameEhlert.model.WordleWord import WordleWord
+
+'''
+GLOBAL VARIABLE CODE BELOW HERE
+'''
+# create global valid_word_set
+valid_word_set = {}
+with open('wordle_word_list.txt') as file:
+    # create empty list
+    word_list = []
+
+    # read each word from file and place in list
+    for line in file:
+        line = line.strip()
+        try:
+            word_list.append(line.upper())
+        except:
+            print("Failed to convert to string")
+
+valid_word_set = set(word_list)
+#print(valid_word_set)
+
+# create global WordleGame object
+wordle_game = WordleGame(valid_word_set)
+
+# generate first attempt in wordle_game
+wordle_game.generate_random_attempt()
+
+'''
+GLOBAL VARIABLE CODE BELOW HERE
+'''
 
 '''
 FUNCTIONS BELOW HERE
 '''
 def start_game():
     """
-    This method is called to start the wordle game. It calls other methods to run the wordle game
-
+    This method enables the entry_guess field and submit_btn and also populates the lbl_current_word_num label text
     :return:
     """
-    # create valid word set
-    valid_word_set = read_word_list_file()
-
-    # create dictionary of WordleWord objects from words in set
-    valid_word_dict = {}
-    id_num = 1
-    for word in valid_word_set:
-        wordle_word_object = WordleWord(word)
-        valid_word_dict[id_num] = wordle_word_object
-        id_num += 1
-
-    # create sentinel value (only used for testing)
-    game_sentinel_value = "-1"
-
-    # create user_guess variable for comparison against sentinel_value
-    user_guess = "0"
-
-    # create words_attempted variable for use in controlling while loop
-    words_attempted = 0
-
-    # assign word_dict length to variable to use for while loop and random number limit
-    word_dict_length = len(valid_word_dict)
-
-    while words_attempted < word_dict_length and user_guess != game_sentinel_value:
-        # use random number to grab object from dictionary
-        random_key = random.randint(1, word_dict_length)
-
-        # create empty set of numbers/keys to store numbers already used
-        used_keys = set()
-
-        # create variable to signify valid random_key
-        random_key_valid = False
-
-        while not random_key_valid:
-            # select object based on number/key from dictionary and check for attempted
-            if valid_word_dict[random_key].attempted:
-                used_keys.add(random_key)
-                random_key = random.randint(1, word_dict_length)
-
-                # while new random number/key in used set generate new number
-                while random_key in used_keys:
-                    random_key = random.randint(1, word_dict_length)
-
-            else:
-                random_key_valid = True
-
-        ### create Attempt object using selected object from dictionary
-        # set WordleWord object._attempted to True
-        valid_word_dict[random_key].set_attempted = True
-
-        # update words_attempted count
-        words_attempted += 1
-
-        # create Attempt object
-        user_attempt = Attempt(valid_word_dict[random_key])
-
-        # extract word from WordleWord object and convert to list
-        char_list = list(user_attempt.wordle_word.word)
-        #print(char_list)
-
-        # create num_of_guesses variable to keep track of guess count
-        num_of_guesses = 1
-
-        # create correct_guess boolean variable for while loop
-        correct_guess = False
-
-        # create wrong_letters list outside of loop to keep track of wrongs letters used
-        wrong_letters = []
-
-        # while loop to check for correct guess or up to five guesses
-        while num_of_guesses <= 5 and correct_guess == False:
-
-            # print guess number
-            print(f"Word #{words_attempted}, Guess #{num_of_guesses}")
-
-            # get user guess
-            user_guess = get_user_guess(valid_word_set)
-
-            # convert user_guess to list
-            user_guess_as_list = list(user_guess)
-            # print(user_guess)
-
-            # create lists for correct_letters and misplaced_letters
-            correct_letters = []
-            misplaced_letters = []
-
-            if user_guess_as_list == char_list:
-                print("Correct")
-                correct_guess = True
-                user_attempt.set_num_of_guesses = num_of_guesses
-                user_attempt.set_guessed_correct = True
-            elif user_guess == game_sentinel_value:
-                print("Game Ended")
-                break
-            elif num_of_guesses <= 4:
-                char_count = 0
-                for char in user_guess_as_list:
-                    if char_list[char_count] == user_guess_as_list[char_count]:
-                        correct_letters.append(char)
-                        # remove char from misplaced letters list if present
-                        if char in misplaced_letters:
-                            misplaced_letters.remove(char)
-                        char_count += 1
-                    elif char in char_list and char not in correct_letters:
-                        correct_letters.append("_")
-                        misplaced_letters.append(char)
-                        char_count += 1
-                    else:
-                        correct_letters.append("_")
-                        wrong_letters.append(char)
-                        char_count += 1
-                # convert letter lists to sets to remove duplicates
-                print("Incorrect")
-                print(f"Correct Letters: {correct_letters}")
-                print(f"Misplaced Letters: {set(misplaced_letters)}")
-                print(f"Wrong Letters: {set(wrong_letters)}")
-
-                num_of_guesses += 1
-            else:
-                print(f"Incorrect, Correct word was{char_list}")
-                num_of_guesses += 1
-                user_attempt.set_num_of_guesses = num_of_guesses
-
-
-def read_word_list_file():
-    """
-    This method is called to read a .txt file containing valid wordle words and put them into a list
-
-    :return: set of valid wordle words
-    """
-    with open('wordle_word_list.txt') as file:
-        # create empty list
-        word_list = []
-
-        # read each word from file and place in list
-        for line in file:
-            line = line.strip()
-            try:
-                word_list.append(line.upper())
-            except:
-                print("Failed to convert to string")
-    # return list as set
-    return set(word_list)
-
-
-def get_user_guess(valid_word_set):
-    """
-    This method is used to prompt the user for the next word they would like to guess
-
-    :param valid_word_set: set of valid words to be used for user input validation
-    :return: word that is to be used as user's guess
-    """
-
-    def valid_guess(user_guess, valid_word_set):
-        """
-        This method is used to check if the user's guess is valid
-
-        :param user_guess: word to be checked if valid
-        :param valid_word_set: set of valid words
-        :return: boolean if user guess is valid
-        """
-        user_guess = user_guess.upper()
-        # check if guess is contained in valid_word_dict
-        if user_guess in valid_word_set or user_guess == "-1":
-            return True
-        else:
-            return False
-
-    # prompt user for input
-    user_guess = input("Please enter your guess: ")
-
-    while not valid_guess(user_guess, valid_word_set):
-        user_guess = input("Not a valid word! Please enter your guess: ")
-
-    return user_guess.upper()
+    submit_btn.config(state=NORMAL)
+    entry_guess.config(state=NORMAL)
+    lbl_current_word_num.config(text= wordle_game.num_of_words_attempted)
+    lbl_num_guessed_correct.config(text= wordle_game.num_guessed_correct)
 
 def create_row_2_letters():
     """
@@ -327,11 +186,93 @@ def change_letter_lbl(letter_lbl):
     """
     letter_lbl.config(relief=SUNKEN)
 
+def submit_guess():
+
+    user_guess = entry_guess.get()
+    print(user_guess)
+
+    # check submission to ensure it is valid
+    if user_guess.upper() not in valid_word_set:
+        lbl_submitted_guess.config(text="Invalid Entry", fg= "red")
+        return
+
+    # check guess for match
+    if (wordle_game.user_attempt.guessed_correct == False) and (wordle_game.user_attempt.num_of_guesses <= 5):
+        # increment the number of guesses by one
+        wordle_game.user_attempt.set_num_of_guesses += 1
+
+        #print(wordle_game.user_attempt.wrong_letters)
+
+        # extract word from WordleWord object and convert to list
+        char_list = list(wordle_game.user_attempt.wordle_word.word)
+        print(char_list)
+
+        # convert user_guess to list
+        user_guess_as_list = list(user_guess.upper())
+
+        # create lists for correct_letters and misplaced_letters
+        correct_letters = []
+        misplaced_letters = []
+
+        if user_guess_as_list == char_list:
+            # update lbl_submitted_guess text
+            label_text = f"Guess #{wordle_game.user_attempt.num_of_guesses} : {user_guess.upper()} is correct!"
+            lbl_submitted_guess.config(text= label_text, fg="green")
+
+            # update guessed_correct of attempt to True
+            wordle_game.user_attempt.set_guessed_correct = True
+
+            # update lbl_num_guessed_correct
+            wordle_game.set_num_guessed_correct += 1
+            lbl_num_guessed_correct.config(text= wordle_game.num_guessed_correct)
+
+            # create new attempt
+            wordle_game.generate_random_attempt()
+
+            # update lbl_current_word_num field and wordle_game number of games attempted
+
+            lbl_current_word_num.config(text=wordle_game.num_of_words_attempted)
+
+        elif wordle_game.user_attempt.num_of_guesses <= 4:
+            char_count = 0
+            for char in user_guess_as_list:
+                if char_list[char_count] == user_guess_as_list[char_count]:
+                    correct_letters.append(char)
+                    # remove char from misplaced letters list if present
+                    if char in misplaced_letters:
+                        misplaced_letters.remove(char)
+                    char_count += 1
+                elif char in char_list and char not in correct_letters:
+                    correct_letters.append("_")
+                    misplaced_letters.append(char)
+                    char_count += 1
+                else:
+                    correct_letters.append("_")
+                    wordle_game.user_attempt.wrong_letters.append(char)
+                    char_count += 1
+            # convert letter lists to sets to remove duplicates
+            print("Incorrect")
+            print(f"Correct Letters: {correct_letters}")
+            print(f"Misplaced Letters: {set(misplaced_letters)}")
+            print(f"Wrong Letters: {set(wordle_game.user_attempt.wrong_letters)}")
+
+            # update lbl_submitted_guess text
+            label_text = f"Guess #{wordle_game.user_attempt.num_of_guesses} : {user_guess.upper()} is incorrect"
+            lbl_submitted_guess.config(text= label_text, fg="black")
+        else:
+            # update lbl_submitted_guess text
+            lbl_submitted_guess.config(text=f"Incorrect, Correct word was{char_list}", fg="red")
+
+            # create new attempt
+            wordle_game.generate_random_attempt()
+
+            #update lbl_current_word_num field and wordle_game number of games attempted
+            lbl_current_word_num.config(text=wordle_game.num_of_words_attempted)
+
+
 '''
 FUNCTIONS ABOVE HERE
 '''
-
-# create main window
 main_window = tk.Tk()
 
 '''
@@ -373,10 +314,6 @@ create_row_3_letters()
 
 # create letter labels for row 4
 row4_letter_lbls = create_row_4_letters()
-
-# line to test change_letter_lbl method
-#TODO
-change_letter_lbl(row4_letter_lbls[0])
 
 # add empty label for row 5
 lbl_empty_row5 = tk.Label(main_window, text='')
@@ -503,21 +440,31 @@ lbl_empty_row15 = tk.Label(main_window, text='')
 lbl_empty_row15.grid(row=15, column=0)
 
 # add row 15 bottom edge borders
+#TODO
 
 # add empty label for row 16
 lbl_empty_row16 = tk.Label(main_window, text='')
 lbl_empty_row16.grid(row=16, column=0)
 
+# create "Word#" label and current word number field in row 17
+lbl_static_word_num = tk.Label(main_window, text="Word # :", justify="right")
+lbl_static_word_num.grid(row=17, column=1, rowspan=1, columnspan=1)
+
+lbl_current_word_num = tk.Label(main_window, text="")
+lbl_current_word_num.grid(row=17, column=2, rowspan=1, columnspan=1)
+
 # create "Enter Guess" label and text entry field in row 17
 lbl_enter_guess = tk.Label(main_window, text="Enter Guess:", justify="right")
 lbl_enter_guess.grid(row=17, column=3, rowspan=1, columnspan=2)
-entry_guess = tk.Entry(main_window, width=20, justify="center")
+
+entry_guess = tk.Entry(main_window, width=20, justify="center", state=DISABLED)
 entry_guess.grid(row=17, column=5, rowspan=1, columnspan=2)
 
 # create "Guessed Correctly" label and number of correctly guess field/label in row 17
 lbl_guessed_correctly = tk.Label(main_window, text="Guessed Correctly:", justify="right")
 lbl_guessed_correctly.grid(row=17, column=8, rowspan=1, columnspan=2)
-lbl_num_guessed_correct = tk.Label(main_window, text="0 FILLER", justify="right")
+
+lbl_num_guessed_correct = tk.Label(main_window, text="", justify="right")
 lbl_num_guessed_correct.grid(row=17, column=10, rowspan=1, columnspan=1)
 
 # add empty label for row 18
@@ -525,7 +472,7 @@ lbl_empty_row18 = tk.Label(main_window, text='')
 lbl_empty_row18.grid(row=18, column=0)
 
 # add Submit Button to row 19
-submit_btn = tk.Button(main_window, text="Submit", width=25, justify="center")
+submit_btn = tk.Button(main_window, text="Submit", width=25, justify="center", state=DISABLED, command=lambda: submit_guess())
 submit_btn.grid(row=19, column=5, rowspan=1, columnspan=2)
 
 # add empty label for row 20
@@ -533,12 +480,16 @@ lbl_empty_row20 = tk.Label(main_window, text='')
 lbl_empty_row20.grid(row=20, column=0)
 
 # add Start Game Button to row 21
-start_game_btn = tk.Button(main_window, text="Start Game")
-start_game_btn.grid(row= 21, column=2, rowspan=1, columnspan=2)
+start_game_btn = tk.Button(main_window, text="Start Game", command= lambda : start_game())
+start_game_btn.grid(row= 21, column=2, rowspan=1, columnspan=1)
+
+# add submitted_guess label to row 21
+lbl_submitted_guess = tk.Label(main_window, text="")
+lbl_submitted_guess.grid(row=21, column=4, rowspan=1, columnspan=4)
 
 # create Quit Game button to row 21
 quit_btn = tk.Button(main_window, text="Quit Game", command=main_window.destroy)
-quit_btn.grid(row=21, column=9, rowspan=1, columnspan=2)
+quit_btn.grid(row=21, column=9, rowspan=1, columnspan=1)
 
 # add empty label for row 22
 lbl_empty_row22 = tk.Label(main_window, text='')
