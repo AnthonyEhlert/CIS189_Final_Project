194417127202703e89e734bc186245efb516d6f7
Tony Ehlert <aehlert@dmacc.edu>
2023-04-25 14:45:03 -0500

    initial commit

---
+++ WordleGameEhlert/constants.py
@@ -0,0 +1,11 @@
+"""
+Program: constants.py
+Author: Tony Ehlert
+Last date modified: 4/25/2023
+
+The purpose of this program is store constant variables that are used within the WordleGameEhlert project
+The input is variables and values to be used as constants
+The output is none
+"""
+
+MAX_GUESSES= 5
